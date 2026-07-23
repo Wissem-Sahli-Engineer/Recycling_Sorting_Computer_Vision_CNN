ba666eeddf4c05b1432dfa299464967a59dceb67
Wissem Sahli <contact.ing.wissem@gmail.com>
2026-07-23 17:22:48 +0100

    0.952 acc Top-1

--- main.py
+++ main.py
@@ -7,4 +7,15 @@ model = YOLO("yolo26n-cls.pt")
 
 
 
-model.train(data="dataset", epochs=25, imgsz=256)
+# model.train(data="dataset", epochs=25, imgsz=256 , device="mps",)
+
+
+model.train(
+    data="dataset",
+    epochs=25,
+    imgsz=256, 
+    batch=64,  
+    device="mps", 
+    workers=8,  # Faster CPU data loading
+    cache=True,  # Cache dataset to RAM
+)
